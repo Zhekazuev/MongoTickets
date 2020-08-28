d47cf745b69dc206f2fa2ab270c30fed9a9dec54
Evgeny Zuev <zueven@mts.by>
2020-08-28 12:54:32 +0300

    Basic logic developed - Main logic with conditions - Read CSV file with tickets - MongoDB Update method - MongoDB Insert method - MongoDB Delete method

--- ot_cases.py
+++ ot_cases.py
@@ -11,28 +11,71 @@ def read_file():
     with open('OT Tickets.csv') as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
         line_count = 0
-        for row in csv_reader:
+    return csv_reader
 
 
-def update():
-    pass
+def update(mycol, row):
+    if row["Ответ MTS"] == "Открыт":
+        status = "Opened"
+    else:
+        status = "Closed"
+    myquery = {"RT №": row["№ кейса в RT"]}
+    newvalues = {"$set": {"Device_type": row["Оборудование"],
+                          "Priority": row["Приоритет"],
+                          "Started_date": row["Дата (открыт)"],
+                          "Temporary_solution_date": row["Дата (должен быть закрыт)"],
+                          "Closing_date": row["Дата (закрыт)"],
+                          "Status": status}}
+
+    x = mycol.update_one(myquery, newvalues)
+    print(x)
 
 
-def insert():
-    pass
+def insert(mycol, row):
+    if row["Ответ MTS"] == "Открыт":
+        status = "Opened"
+    else:
+        status = "Closed"
+
+    mydict = {
+        "RT №": row["№ кейса в RT"],
+        "Case_title": row["Краткое описание"],
+        "Device_type": row["Оборудование"],
+        "S/W_version": "",
+        "Priority": row["Приоритет"],
+        "Description": "",
+        "Started_date": row["Дата (открыт)"],
+        "Temporary_solution_date": row["Дата (должен быть закрыт)"],
+        "Closing_date": row["Дата (закрыт)"],
+        "Contract_number": "1410-18/ЗП",
+        "Comments": "",
+        "Status": status,
+        "Opener": ""}
+    x = mycol.insert_one(mydict)
+    print(x)
+
+
+def delete(mycol, id):
+    mycol.delete_one({"_id": id})
 
 
 def main():
-    cloud_file = read_file()
-    myclient = pymongo.MongoClient(mongo_ip)
-    mydb = myclient[mongo_db]
-    mycol = mydb[mongo_collection]
-    records = mycol.find({"RT №": "90103287"})
-    for record in records:
-        if record:
-            update()
-        else:
-            insert()
+    with open('OT Tickets.csv', encoding="utf8") as csv_file:
+        csv_reader = csv.DictReader(csv_file, delimiter=',')
+
+        myclient = pymongo.MongoClient(mongo_ip)
+        mydb = myclient[mongo_db]
+        mycol = mydb[mongo_collection]
+
+        for row in csv_reader:
+            records = mycol.find({"RT №": row["№ кейса в RT"]})
+            if records.count() == 0:
+                insert(mycol, row)
+            elif records.count() == 1:
+                update(mycol, row)
+            elif records.count() == 2:
+                delete(mycol, records[0]["_id"])
+                update(mycol, row)
 
 
 if __name__ == '__main__':
